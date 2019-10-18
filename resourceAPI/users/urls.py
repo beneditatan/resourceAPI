d3030040909cec30e2172f637c6e8fbc77a516d8
Benedita Tanabi <beneditatan@Beneditas-MacBook-Pro.local>
2019-10-18 22:22:18 +0800

    get user detail api

--- resourceAPI/users/urls.py
+++ resourceAPI/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Catch-all path to spit anything to lead.
     re_path(r'^$', views.get_user_list, name='get_user_list'),
+    re_path(r'^(?P<username>[\w-]+)/$', views.user_detail, name='user_detail'),
 ]
--- resourceAPI/users/views.py
+++ resourceAPI/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -8,8 +9,25 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view(["GET"])
 def get_user_list(request):
+    if request.method != "GET":
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
     users = User.objects.all()
-    print(users)
     serializer = UserSerializer(users, many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET", "PUT"])
+def user_detail(request, username):
+    if request.method == "GET":
+        user = get_object_or_404(User, username=username)
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+
+        return Response({"detail": "Creation successful"}, status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
