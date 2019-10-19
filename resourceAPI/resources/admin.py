f2e7a1eb96ed5b369033642142c0871f3ba2f25f
Benedita Tanabi <beneditatan@Beneditas-MacBook-Pro.local>
2019-10-19 20:58:23 +0800

    added resource tables to db

--- resourceAPI/resources/admin.py
+++ resourceAPI/resources/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Resource, ResourceQuota
 
 # Register your models here.
+admin.site.register(Resource)
+admin.site.register(ResourceQuota)
--- resourceAPI/resources/models.py
+++ resourceAPI/resources/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ResourceQuota(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quota = models.IntegerField(default=0)
+
+
+class Resource(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
